cd009f35f02ae7b1909b34f864ea8ec20ee26eb0
Mohammad Babamahmoudi <mohammad.babamahmoudi@snapp.cab>
2019-07-12 16:51:58 +0430

    added dotenv

--- tests/api/RateLimiterCest.php
+++ tests/api/RateLimiterCest.php
@@ -5,9 +5,7 @@ class RateLimiterCest
     public function gist_rate_rimiter_test(ApiTester $user){
 
         $user->wantToTest("gists rate limiter");
-
         $user->sendGET(getenv("GIST_BASE_URL"));
-//        $user->sendGET("https://api.github.com");
         $user->seeResponseCodeIs(200);
 
 
